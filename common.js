493817b29abdc2fc3a3a241fd831a8c5828b7d00
larabegale <128634525+larabegale@users.noreply.github.com>
2024-09-23 15:44:44 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -7,6 +7,13 @@ const tickConfig = {
     family: getCSS('--font'),
     size: 16,
     color: getCSS('--primary-color')
+};
+
+function incluirTexto(texto) {
+    const container = document.getElementById('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    container.appendChild(paragrafo)
 }
 
-export {getCSS, tickConfig}
+export { getCSS, tickConfig, criarGrafico, incluirTexto }
